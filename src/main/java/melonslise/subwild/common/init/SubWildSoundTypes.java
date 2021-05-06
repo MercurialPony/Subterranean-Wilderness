f40b4fde5cbbd1ce92464f2e93e45eb9411f7b10
Oscar N <PaintNinja@users.noreply.github.com>
2021-05-06 20:16:35 +0100

    Add ability to craft dirt stairs and slabs, closes #41
    Also replaced the deprecated SoundType with a ForgeSoundType equivalent

--- src/main/java/melonslise/subwild/common/init/SubWildSoundTypes.java
+++ src/main/java/melonslise/subwild/common/init/SubWildSoundTypes.java
@@ -2,8 +2,12 @@ package melonslise.subwild.common.init;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.util.SoundEvents;
+import net.minecraftforge.common.util.ForgeSoundType;
 
 public final class SubWildSoundTypes
 {
-	public static final SoundType WATER = new SoundType(1f, 1f, SoundEvents.ENTITY_GENERIC_SPLASH, SoundEvents.ENTITY_GENERIC_SWIM, SoundEvents.ENTITY_GENERIC_SPLASH, SoundEvents.ENTITY_BOAT_PADDLE_WATER, SoundEvents.ENTITY_GENERIC_SPLASH);
+	public static final SoundType WATER =
+			new ForgeSoundType(1f, 1f, () -> SoundEvents.ENTITY_GENERIC_SPLASH,
+					() -> SoundEvents.ENTITY_GENERIC_SWIM, () -> SoundEvents.ENTITY_GENERIC_SPLASH,
+					() -> SoundEvents.ENTITY_BOAT_PADDLE_WATER, () -> SoundEvents.ENTITY_GENERIC_SPLASH);
 }
